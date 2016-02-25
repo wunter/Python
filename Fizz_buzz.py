c74ba02b153afa7f863cf86ad3984aa8f7c5738f
wunter <franciscojavier.perezlopez@alum.uca.es>
2016-02-25 22:05:06 +0100

    Update Fizz_buzz.py

--- Fizz_buzz.py
+++ Fizz_buzz.py
@@ -14,15 +14,16 @@ Salida: La respuesta como una cadena de caracteres.
 
 def fizz_buzz (number):
   
-  answer = number
-  
-  if number % 3 == 0 and number % 5 == 0:
-      answer = "Fizz Buzz"
-      
-  elif number % 3 == 0:
-      answer = "Fizz"
-      
-  elif number % 5 == 0:
-      answer = "Buzz"
-        
-  return answer
+    #Escriba la solución aquí
+    
+
+    
+    return 
+
+
+# Comprobar si la solución es correcta
+fizz_buzz (15) # "Fizz Buzz", "15 is divisible by 3 and 5"
+fizz_buzz (6) # "Fizz", "6 is divisible by 3"
+fizz_buzz (5) # "Buzz", "5 is divisible by 5"
+
+
