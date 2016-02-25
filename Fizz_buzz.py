e6c7e04a84dd578e49b0d9be885e2940a812987c
wunter <franciscojavier.perezlopez@alum.uca.es>
2016-02-25 20:44:14 +0100

    Create Fizz_buzz.py

---
+++ Fizz_buzz.py
@@ -0,0 +1,13 @@
+
+""" 
+"Fizz buzz" 
+
+Escribe una funcion que reciba un numero entero y deverá devolver:
+- "Fizz Buzz" si el número es divisible por 3 y por 5;
+- "Fizz" si el número es divisible por 3;
+- "Buzz" si el número es divisible por 5; 
+
+Entrada: Un número como un entero.
+
+Salida: La respuesta como una cadena de caracteres.
+"""
