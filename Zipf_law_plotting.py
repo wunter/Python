3245043145d110a9658ba9f716b29c9be931c7ca
wunter <franciscojavier.perezlopez@alum.uca.es>
2016-02-28 16:32:59 +0100

    Update Zipf_law_plotting.py

--- Zipf_law_plotting.py
+++ Zipf_law_plotting.py
@@ -1,7 +1,7 @@
 # Esta función toma un diccionario con la distribución de frecuencia y plotea la ley de Zipf
 
 
-def zipf_law (dist_frec, scale = 'log'):
+def zipf_law (dist_frec, scale = 'log'): # Cambiar a False para desactivar
 	
 	frecuencia = [i for i in dist_frec.values()]
 	
@@ -15,13 +15,14 @@ def zipf_law (dist_frec, scale = 'log'):
 	
 	if scale:
 		pyplot.xscale(scale)
-    		pyplot.yscale(scale)
+    pyplot.yscale(scale)
     		
-    	pyplot.title('Zipf plot')
+  pyplot.title('Zipf plot')
 	pyplot.xlabel('Rank')
-    	pyplot.ylabel('Frequency')
-    	pyplot.plot(rs, fs, 'r-')
-    	return pyplot.show()
+  pyplot.ylabel('Frequency')
+  pyplot.plot(rs, fs, 'r-')
+    	
+  return pyplot.show()
 		
 	
 
